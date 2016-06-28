ef87a672d6c7b2381909a4a210434183302bfcbd
helios <lhitaliani@gmail.com>
2016-06-28 06:15:33 -0300

    Lectura archivos estaticos

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var http = require('http');
+var fs = require('fs');
+
+urls = {'/': './static/index.html'};
 
 http.createServer(function(request, response) {
-  response.write('Holaaaa');
-  response.end();
+
+  if( request.url in urls ) {
+    fs.readFile(urls[request.url], function(err,html) {
+      response.write(html);
+      response.end();
+    })
+  }
 }).listen(8080);
